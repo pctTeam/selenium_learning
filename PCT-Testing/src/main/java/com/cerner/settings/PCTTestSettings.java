ca34b0394ae234d82da577a5f015ef3400a5e553
G,Rajesh <Rajesh.G@Cerner.com>
2017-03-12 21:37:15 +0530

    initail push

---
+++ PCT-Testing/src/main/java/com/cerner/settings/PCTTestSettings.java
@@ -0,0 +1,25 @@
+/* Identify WebElements for use on the Template Page and any methods unique to action on this page */
+
+//Note this page is just a copy example of using the login page 
+
+package main.java.com.cerner.settings;
+
+import java.io.File;
+import main.java.com.cerner.extentReporting.*;
+
+public class PCTTestSettings extends ExtentReporting {
+  
+  public static void GetStarted() {
+    browserName = "chrome" ;
+    String OS = System.getProperty("os.name");
+    
+    if (OS.contains("Windows")) {
+        reportPath = File.separator + File.separator + "hnagroup" + File.separator + "PMO"
+                + File.separator + "Program Management" + File.separator + "Project GO" + File.separator
+                + "Testing" + File.separator + "TestNG Selenium Project Reports";
+      }
+      if (OS.equals("Mac OS X")) {
+          reportPath = "smb://hnagroup/PMO/Program Management/Project GO/Testing/TestNG Selenium Project Reports";
+      }
+  }
+}
